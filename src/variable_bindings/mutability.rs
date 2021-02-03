7ab9d347ab8e4d7fff0551d93df8bbc2a5353675
Vicente <vicentedpsantos@gmail.com>
2021-02-03 10:19:53 -0300

    mutability

---
+++ src/variable_bindings/mutability.rs
@@ -0,0 +1,14 @@
+// Variable bindings are immutable by default, but this can
+// be overridden using the mut modifier
+
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // OK
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+}

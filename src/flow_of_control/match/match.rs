f6a5032712258c21a005ea1d71ee15f780fcc92d
Vicente <vicentedpsantos@gmail.com>
2021-02-05 11:35:42 -0300

    match

---
+++ src/flow_of_control/match/match.rs
@@ -0,0 +1,28 @@
+// Rust provides a pattern matching via the `match` keyword, which can
+// be used like a C `switch`
+
+fn main() {
+    let number = 13;
+
+    println!("Tell me about {}", number);
+
+    match number {
+        // Match a single value
+        1 => println!("One"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        // Match an inclusive range
+        13..=19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+    }
+
+    let boolean = true;
+
+    let binary = match boolean {
+        false => 0,
+        true => 1,
+    };
+
+    println!("{} -> {}", boolean, binary);
+}
